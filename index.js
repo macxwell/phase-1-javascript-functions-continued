5f33139e177908941424b28d0ee1d46eb51bf6fb
macxwell <omondimacxwell@gmail.com>
2024-07-18 12:20:11 +0300

    continuation of function labs

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code your solution here
+
+
+// Saturday fun function declaration
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Monday work function expression
+  let mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Wrap adjective function
+  function wrapAdjective(symbol = '*') {
+    return function(adjective) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+    }
+  }
+  
+  // Exporting functions for testing
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+  };
+  // helpers.js
+
+const { saturdayFun, mondayWork, wrapAdjective } = require('./index');
+
+describe("index.js", () => {
+  // Your existing test suite goes here
+});
